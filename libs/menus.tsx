6df190ac2920fcac066078a5742235ad5f17ab12
William Tso <vitalitymail@foxmail.com>
2024-05-26 14:59:10 +0800

    change nav menu

--- libs/menus.tsx
+++ libs/menus.tsx
@@ -1,21 +1,147 @@
 export const menus = [
+  // {
+  //   label: "Home",
+  //   link: "#",
+  //   submenu: [
+  //     {
+  //       label: "Home Demo - 1",
+  //       link: "/",
+  //     },
+  //     {
+  //       label: "Home Demo - 2",
+  //       link: "/home-2/",
+  //     },
+  //     {
+  //       label: "Home Demo - 3",
+  //       link: "/home-3/",
+  //     },
+  //   ],
+  // },
+  
+  // {
+  //   label: "Services",
+  //   link: "#",
+  //   submenu: [
+  //     {
+  //       label: "Services Style - 1",
+  //       link: "/services/",
+  //     },
+  //     {
+  //       label: "Services Style - 2",
+  //       link: "/services/two/",
+  //     },
+  //     {
+  //       label: "Service Details",
+  //       link: "/services/details/",
+  //     },
+  //   ],
+  // },
+  // {
+  //   label: "Projects",
+  //   link: "#",
+  //   submenu: [
+  //     {
+  //       label: "Projects",
+  //       link: "/projects/",
+  //     },
+  //     {
+  //       label: "Projects Details",
+  //       link: "/projects/details/",
+  //     },
+  //   ],
+  // },
+  // {
+  //   label: "Pages",
+  //   link: "#",
+  //   submenu: [
+  //     {
+  //       label: "Pricing",
+  //       link: "/pricing/",
+  //     },
+  //     {
+  //       label: "Team",
+  //       link: "/team/",
+  //     },
+  //     {
+  //       label: "Faq",
+  //       link: "/faq/",
+  //     },
+  //     {
+  //       label: "Career",
+  //       link: "/career/",
+  //     },
+  //     {
+  //       label: "Career Details",
+  //       link: "/career/details/",
+  //     },
+  //     {
+  //       label: "Profile Authentication",
+  //       link: "/profile-authentication/",
+  //     },
+  //     {
+  //       label: "Membership Levels",
+  //       link: "/membership-levels/",
+  //     },
+  //     {
+  //       label: "Terms of Service",
+  //       link: "/terms-of-service/",
+  //     },
+  //     {
+  //       label: "Privacy Policy",
+  //       link: "/privacy-policy/",
+  //     },
+  //     {
+  //       label: "Coming Soon",
+  //       link: "/coming-soon/",
+  //     },
+  //   ],
+  // },
+  
+  // {
+  //   label: "Pricing",
+  //   link: "/pricing/",
+  // },
+  // {
+  //   label: "Contact",
+  //   link: "/contact/",
+  // },
   {
-    label: "Home",
+    label:"Our tech",
+    link:"#"
+  },
+  {
+    label:"AX",
+    link:"#"
+  },
+  {
+    label: "News",
     link: "#",
     submenu: [
       {
-        label: "Home Demo - 1",
-        link: "/",
+        label: "Blog Style - 1",
+        link: "/blog/",
       },
       {
-        label: "Home Demo - 2",
-        link: "/home-2/",
+        label: "Blog Style - 2",
+        link: "/blog/two/",
       },
       {
-        label: "Home Demo - 3",
-        link: "/home-3/",
+        label: "Blog Right Sidebar",
+        link: "/blog/with-right-sidebar/",
+      },
+      {
+        label: "Blog Details",
+        link: "/blog/details/",
+      },
+      {
+        label: "Blog Details With Video",
+        link: "/blog/details/2/",
       },
     ],
+  },
+  {
+    label:"Join us",
+    link:"/career"
   },
   {
     label: "About",
@@ -42,117 +168,5 @@ export const menus = [
         link: "/testimonials/",
       },
     ],
-  },
-  {
-    label: "Services",
-    link: "#",
-    submenu: [
-      {
-        label: "Services Style - 1",
-        link: "/services/",
-      },
-      {
-        label: "Services Style - 2",
-        link: "/services/two/",
-      },
-      {
-        label: "Service Details",
-        link: "/services/details/",
-      },
-    ],
-  },
-  {
-    label: "Projects",
-    link: "#",
-    submenu: [
-      {
-        label: "Projects",
-        link: "/projects/",
-      },
-      {
-        label: "Projects Details",
-        link: "/projects/details/",
-      },
-    ],
-  },
-  {
-    label: "Pages",
-    link: "#",
-    submenu: [
-      {
-        label: "Pricing",
-        link: "/pricing/",
-      },
-      {
-        label: "Team",
-        link: "/team/",
-      },
-      {
-        label: "Faq",
-        link: "/faq/",
-      },
-      {
-        label: "Career",
-        link: "/career/",
-      },
-      {
-        label: "Career Details",
-        link: "/career/details/",
-      },
-      {
-        label: "Profile Authentication",
-        link: "/profile-authentication/",
-      },
-      {
-        label: "Membership Levels",
-        link: "/membership-levels/",
-      },
-      {
-        label: "Terms of Service",
-        link: "/terms-of-service/",
-      },
-      {
-        label: "Privacy Policy",
-        link: "/privacy-policy/",
-      },
-      {
-        label: "Coming Soon",
-        link: "/coming-soon/",
-      },
-    ],
-  },
-  {
-    label: "Blog",
-    link: "#",
-    submenu: [
-      {
-        label: "Blog Style - 1",
-        link: "/blog/",
-      },
-      {
-        label: "Blog Style - 2",
-        link: "/blog/two/",
-      },
-      {
-        label: "Blog Right Sidebar",
-        link: "/blog/with-right-sidebar/",
-      },
-      {
-        label: "Blog Details",
-        link: "/blog/details/",
-      },
-      {
-        label: "Blog Details With Video",
-        link: "/blog/details/2/",
-      },
-    ],
-  },
-  {
-    label: "Pricing",
-    link: "/pricing/",
-  },
-  {
-    label: "Contact",
-    link: "/contact/",
   },
 ];
